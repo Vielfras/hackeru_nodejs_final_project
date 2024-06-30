07bc9e926f9e95ae2065f09135cec932e2c66d38
vielfras <vielfras@gmail.com>
2024-06-30 21:58:53 +0300

    bug fix and deletion of comments

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -24,7 +24,7 @@ const getUserById = async (req, res) => {
   const { id } = req.params;
   
   try {
-    const authError = isAdminOrCreator(req.user, id, res);
+    const authError = Auth.isAdminOrCreator(req.user, id, res);
     if (authError) {
       return authError;
     } 
@@ -42,64 +42,6 @@ const getUserById = async (req, res) => {
     return res.status(400).json({ success: false, message: "Invalid format for user id.", });
   }
 };
-
-// const searchInUsers = async (req, res) => {
-//
-//   // validate the request's body using joi
-//   const { error, value } = schemas.searchUser.validate(req.body);
-//   // check if there are joi validation errors
-//   if (error) {
-//     const errorsArray = error.details.map((err) => err.message); // creates an array of error-message strings
-//     return res.status(400).json({ success: false, message: errorsArray });
-//   }
-//   // destructuring the variables from 'value'
-//   const { searchTerm, searchFields } = value;
-//
-//   try {
-//     // find the users containing the 'searchTerm' using our static 'multipleFieldsStringSearch' method
-//     const found = await User.multipleFieldsStringSearch(searchTerm,searchFields);
-//     // return the results (an empty array if not found)
-//     return res.status(found.length !== 0 ? 200 : 204).json({
-//       success: true,
-//       data: found,
-//     });
-//   } catch (err) {
-//     // return an error message
-//     return res.status(400).json({ success: false, message: err.message });
-//   }
-// };
-
-// TODO - This is a duplicate of auth/register
-// const createNewUser = async (req, res) => {
-//   const { error, value } = schemas.createNewUser.validate(req.body);
-//   if (error) {
-//     return res.status(400).json(Err.multipleErrToString(error));
-//   }
-
-//   const newUser = new User(value);
-
-//   try {
-//     const hashedPassword = await bcrypt.hash(newUser.password, 10);
-//     newUser.password = hashedPassword;
-
-//     const saved = await newUser.save();
-//     const savedObject = saved.toObject();
-//     // Delete 'password' key before returning object to user
-//     delete savedObject.password;
-
-//     return res.status(201).json({
-//       success: true,
-//       created: savedObject,
-//     });
-//   } catch (err) {
-//     // handle duplicate email
-//     if (err.code === 11000) {
-//       return res.status(409)
-//         .json({ success: false, message: `Email ${newUser.email} is already registered! Consider logging in.` })
-//     }
-//     return res.status(500).json({ success: false, message: `Error saving the user.` });
-//   }
-// };
 
 const deleteUser = async (req, res) => {
   const { id } = req.params;
@@ -157,9 +99,8 @@ const updateUser = async (req, res) => {
 
 
 const updateUserBusinessStatus = async (req, res) => {
-  console.log(req.body);
   const { error, value } = schemas.updateUserBusinessStatus.validate(req.body);
-
+  
   if (error) {
     return res.status(400).json(Err.multipleErrToString(error));
   }
@@ -189,8 +130,6 @@ const updateUserBusinessStatus = async (req, res) => {
 module.exports = {
   getAllUsers,
   getUserById,
-  //searchInUsers,
-  // createNewUser,
   deleteUser,
   updateUser,
   updateUserBusinessStatus,
